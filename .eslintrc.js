c07096ca11006ac6f7fe0bad0982a6e15ecf1386
Zeeshan Faiz <zeshan.faiz@ebitlogix.com>
2022-01-21 17:22:05 +0500

    smjh tw gaye hogy

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,10 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
     "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
-    "plugins": [
-        "react"
-    ],
     "rules": {
     }
 }
